bb7f1e6c1e4b2732075f4e9336beada509f9e876
shubhra tiwary <shubhratiwary5@gmail.com>
2024-08-09 19:41:23 +0530

    home Page components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 import Navbar from "./Components/Navbar";
+import { Box, Button, Container, TextField, Typography } from '@mui/material';
 
 const App = () => {
+ 
   return (
     <header>
       <SignedOut>
@@ -12,6 +14,27 @@ const App = () => {
       <SignedIn >
         <div className="flex justify-between mt-5 mx-10">
         <Navbar></Navbar>
+        <Container maxWidth="md" sx={{ mt: 4 }}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        DocWeb
+      </Typography>
+
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 4 }}>
+        <Button variant="contained" color="primary" >
+          My Documents
+        </Button>
+        <Button variant="contained" color="secondary" >
+          Upload  Documents
+        </Button>
+      </Box>
+
+      <TextField
+        fullWidth
+        variant="outlined"
+        placeholder="Search for documents..."
+        
+      />
+    </Container>
         <UserButton />
         </div>
     
