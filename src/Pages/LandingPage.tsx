b47a11e043911931f7c81a48d573c43328b8d9e0
shubhra tiwary <shubhratiwary5@gmail.com>
2024-08-11 10:22:33 +0530

    color for the text in landing page changed

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -7,12 +7,12 @@ const LandingPage = () => {
   return (
     <div className="landing-container min-h-screen flex   ">
       <div >
-        <h1 className="text-7xl  mb-4">Docweb</h1>
-        <p className="text-lg mb-8">Welcome to Docweb - Your go-to solution for document management.</p>
+        <h1 className="text-7xl  mb-4 text-yellow-500">Docweb</h1>
+        <p className="text-lg mb-8 text-yellow-300">Welcome to Docweb - Your go-to solution for document management.</p>
         <SignInButton>
         <a
           href="#get-started"
-          className="bg-red-500 text-white px-6 py-3 rounded-lg text-lg font-semibold hover:bg-blue-600 transition duration-300"
+          className="bg-yellow-500 text-white px-6 py-3 rounded-lg text-lg font-semibold hover:bg-yellow-600 transition duration-300"
         >
           Get Started
         </a>
